a1ade616db3ecaddefa44c1a5be9082ae95b5e6e
NZ0 <nathaniel.v.griffin.dewitt@gmail.com>
2025-06-01 00:56:59 -0500

    Update evade.ps1

--- evade.ps1
+++ evade.ps1
@@ -3,13 +3,13 @@ $wallpaperHash = (Get-FileHash -Path $wallpaperPath -Algorithm SHA256).Hash
 
 # Define a list of known hashes to check against
 $knownHashes = @(
-    "A37491A5E479E191CFE547B484426037455CE8AACFF7AB15F91925330C24864F", # windows default wallpaper hashes
+    "A37491A5E479E191CFE547B484426037455CE8AACFF7AB15F91925330C24864F", # Windows default wallpaper hashes
     "D54262032574B15CCF541E40FA13475FFB39D8682C19383E917F3ED5E8BBCA71"
 )
 
 # Check if the wallpaper hash is in the list
 if ($knownHashes -contains $wallpaperHash) {
-    Write-Output "Has a windows defualt wallpaper contining infection, -This is harmless fyi"
+    Write-Output "Has a Windows default wallpaper containing infection - this is harmless, FYI."
 } else {
-    Write-Output "Did not detect any common windows wallpapers, deleting self"
+    Write-Output "Did not detect any common Windows wallpapers, deleting self."
 }
